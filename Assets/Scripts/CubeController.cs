7fe6df2779655548ef01bb3ada5a4a33c0ba40cd
Simone Alers <simone@letstalk.nl>
2015-09-28 11:53:37 +0200

    Generate cubes in random positions on play

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	public Camera camera;
+	public float cameraDistance; //0 is random
+	public GameObject cubePrefabObject;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 10; i++) { //generate 10 random cubes
+			this.generateNewCube ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//generate a random position within certain bound for a cube to be in
+	Vector3 generateRandomPosition(){
+		//Z is depth here
+		float z = this.camera.transform.position.z + cameraDistance;
+		if (this.cameraDistance == 0) {
+			z += this.randomInBetween(5f, 10f);
+		}
+
+		float x = this.randomInBetween (-10f, 10f); //between left and right walls
+		float y = this.randomInBetween (0f, 5f); //between ground and viewable height
+		return new Vector3 (x, y, z);
+	}
+
+	//generate a new cube in the space
+	void generateNewCube() {
+		GameObject newCube = Instantiate (this.cubePrefabObject);
+		newCube.GetComponent<Renderer> ().enabled = true;
+		newCube.transform.position = this.generateRandomPosition();
+	}
+
+	float randomInBetween(float from, float to){
+		return (Random.value * (to - from)) + from;
+	}
+}
